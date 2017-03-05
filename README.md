7a5d17306ce04fcdb6f6170fb46c7d8244b690e4
C Cesar Nardo Junior <C Cesar Nardo Junior>
2017-03-05 15:46:15 -0300

    Versão 1.0.0 finalizada

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Front-end padrão dos projetos da OBS Digital.
 
 ![Versão](http://obsdigital.com.br/git/svg/versao.svg)
 
-### Detalhes
+## Detalhes
 Esse diretório foi criado para agilizar e automatizar tarefas do nosso Front-end. Para isso, foram usados nesse projeto: Gulp, Bower, Pugjs, além das dependências do NodeJS.
 
 ### Step-by-step
@@ -13,11 +13,7 @@ Todos os passos abaixo serão feitos utilizando o prompt do NodeJS.
 ```
 git clone https://github.com/obsfront/front-end
 ```
-- Instale o Bower
+- Instale as dependências do NodeJS
 ```
-npm install bower
-```
-- Instale as dependências do Bower
-```
-bower install
+npm install
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,55 @@
+// VARIAVEIS
 var gulp = require ('gulp');
 
-gulp.task('default',[]);
+var pug = require('gulp-pug2');
+
+var sass = require ('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+var uglify = require('gulp-uglify');
+
+var image = require('gulp-image');
+
+// TASKS
+gulp.task('pug', function(){
+  return gulp.src('./source/*.pug')
+  .pipe(pug({locals: './source/*.pug'}))
+  .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('sass', function(){
+  return gulp.src('./source/sass/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./source/css/'))
+})
+
+gulp.task('minify', function(){
+  return gulp.src('./source/css/*.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(rename('main.min.css'))
+  .pipe(gulp.dest('./dist/css/'))
+})
+
+gulp.task('uglify', function(){
+  return gulp.src('./source/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('image', function(){
+  return gulp.src('./source/images/*')
+  .pipe(image())
+  .pipe(gulp.dest('./dist/images/'))
+})
+
+// WATCH
+gulp.task('watch', function(){
+  gulp.watch(['./source/*.pug'],['pug']) // pug
+  gulp.watch(['./source/sass/*.scss'],['sass']) // sass
+  gulp.watch(['./source/css/*.css'],['minify']) // minify
+  gulp.watch(['./source/js/*.js'],['uglify']) // uglify
+})
+
+// TASK PRINCIPAL
+gulp.task('default',['image','watch']);
